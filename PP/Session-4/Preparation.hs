3ddcc480ec59250d8256ee129d49bf4b9de4ea72
Pedersen2001 <lucas-l-h-p@hotmail.dk>
2024-10-07 14:34:15 +0200

    Prep-sheet for PP session 4

---
+++ PP/Session-4/Preparation.hs
@@ -0,0 +1,21 @@
+-- Define, using pattern matching and without using the length function, a function onlytwo that tells
+-- us if a list has precisely two elements – in which case it must return True – or not, in which case it
+-- must return False. What is the type of onlytwo?
+
+onlytwo :: [a] -> Bool
+onlytwo [_, _] = True
+onlytwo _      = False
+
+
+
+-- The dot product of two pairs of numbers (a, b) and (c, d) is the number a · c + b · d. Define, using
+-- list comprehension, a function alldots that takes two lists of pairs of numbers and returns all the
+-- possible dot products of every pair from the first list and every pair from the second list. Find two
+-- good test case for testing your function definition and use them to test your code. What is the type
+-- of alldots ?
+
+alldots :: [(Int, Int)] -> [(Int, Int)] -> [Int]
+alldots xs ys = [a * c + b * d | (a, b) <- xs, (c, d) <- ys]
+
+test1 = alldots [(1, 2), (3, 4)] [(5, 6), (7, 8)]
+test2 = alldots [(0, 1), (1, 0)] [(1, 1), (2, 2)]
